6c74ad94f90ee11ede140385bd7ee6ea2adaa8d2
raffyalbar30 <geminglol09@gmail.com>
2026-07-23 16:15:00 +0700

    update refresh token implementation jwt

--- controllers/RefreshToken.js
+++ controllers/RefreshToken.js
@@ -1,5 +1,5 @@
 const connectDB = require("../DB/connections");
-const { verifyRefreshToken, GenerateSecretToken } = require("../services/generateToken");
+const { verifyRefreshToken, GenerateSecretToken, GenerateRefreshToken } = require("../services/generateToken");
 
 
 const AuthRefreshToken = async (req, res) => {
@@ -13,7 +13,6 @@ const AuthRefreshToken = async (req, res) => {
 
    try {
       const decoded = verifyRefreshToken(token);
-      console.log(decoded);
 
       const sql = 'SELECT * FROM refresh_tokens WHERE token = ? AND revoked = FALSE AND expires_at > NOW()'
       connectDB.query(sql, [token], (err, result) => {
@@ -24,24 +23,37 @@ const AuthRefreshToken = async (req, res) => {
         const data = result[0].id_refresh; 
        //   rotate token yang tidak valid menjadi token baru
         const sql = 'UPDATE refresh_tokens SET revoked = TRUE WHERE id_refresh = ?'; 
-        connectDB.query(sql,[data],(err, result) => {
-           return res.status(201).json({
-              message: "sorry token udah diganti"
-           })
-        })
+        connectDB.query(sql,[data])
+
       }); 
 
-   
-      const data2 = decoded.user_id;
       const sql2 = 'SELECT * FROM user_cermat WHERE user_id = ?'; 
+      const data2 = decoded.user_id;
+      
       connectDB.query(sql2, [data2], (req, result) => {
-         console.log(result);
+
+         const results = result[0];
+         const accesToken = GenerateSecretToken(results); 
+         const refreshToken = GenerateRefreshToken(results); 
+
+         connectDB.query( 'INSERT INTO refresh_tokens (id_user, token, expires_at) VALUES (?, ?, DATE_ADD(NOW(), INTERVAL 8 HOUR))',
+                [results.user_id, refreshToken]);
+
+         res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: 8 * 60 * 60 * 1000,
+         });
+
+         res.status(201).json({user: { user_id: results.user_id, email: results.email_user }, accesToken });
       })
+
+
 
    } catch (error) {
       console.log(error);
    }
-
 }
 
 module.exports = {
