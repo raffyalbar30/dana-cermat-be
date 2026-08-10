d6718483f039736dd7c5365311ce949fa259f10c
raffyalbar30 <geminglol09@gmail.com>
2026-08-10 20:20:04 +0700

    " reset password update feature "

---
+++ controllers/Resetpassword.js
@@ -0,0 +1,15 @@
+const connectDB = require("../DB/connections"); 
+
+const Resetpassword = (req, res) => {
+    const { email } = req.body; 
+
+    const sql = `SELECT * FROM user_cermat WHERE email_user = ?`;
+    connectDB.query(sql, [email], (err, result) => {
+         const data = result[0]; 
+         console.log(data); 
+    })
+}
+
+module.exports = {
+    Resetpassword
+}
